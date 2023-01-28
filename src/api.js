7580e0f5bc8d265f181e19736d39318176e52139
Chethan B M <35001882+ChethanShetty563@users.noreply.github.com>
2023-01-28 14:51:12 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,9 +1,35 @@
 
 const express = require("express");
 const serverless = require("serverless-http");
-
+const mongoose = require('mongoose');
+const UserModel = require('./models/Users');
 const app = express();
 const router = express.Router();
+
+
+
+app.use(express.json());
+app.use(cors());
+
+ongoose.connect('mongodb+srv://adi123:adi123@cluster0.x3tto.mongodb.net/?retryWrites=true&w=majority');
+
+router.get('/getUsers', (req, res) => {
+  UserModel.find({}, (err, result) => {
+    if (err) {
+      res.json(err);
+    } else {
+      res.json(result);
+    }
+  });
+});
+
+router.post('/createUser', async (req, res) => {
+  const user = req.body;
+  const newUser = new UserModel(user);
+  await newUser.save();
+
+  res.json(user);
+});
 
 router.get("/", (req, res) => {
   res.json({
